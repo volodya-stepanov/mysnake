397b0dabb9bb59c96fc4b9d79b645306dcc2677f
Volodya <volodya.stepanov.edu@yandex.ru>
2016-07-26 12:59:24 +0300

    Осваиваем класс "Список"

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -16,13 +16,35 @@ namespace MySnake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Console.ReadLine();
-        }
+            Point p3 = new Point(2, 6, '$');
+            p3.Draw();
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            Point p4 = new Point(3, 7, '&');
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            Console.ReadLine();
         }
     }
 }
