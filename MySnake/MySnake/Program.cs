ca4ae78220da1ec878f40325989082e775f6ecd8
Volodya <volodya.stepanov.edu@yandex.ru>
2016-07-27 11:38:40 +0300

    Класс "Змейка"

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -10,16 +10,22 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-            
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Console.SetBufferSize(80, 25);
 
-            //HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            VerticalLine verticalLine = new VerticalLine(8, 5, 10, '+');
-            //line.Draw();
-            verticalLine.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+
+            snake.Draw();
 
             Console.ReadLine();
         }
--- MySnake/MySnake/Snake.cs
+++ MySnake/MySnake/Snake.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace MySnake
 {
-    class VerticalLine : Figure
+    class Snake : Figure
     {
-        public VerticalLine(int yTop, int yBottom, int x, char sym)
+        public Snake(Point tail, int length, Direction direction)
         {
             pList = new List<Point>();
-            for (int y = yTop; y <= yBottom; y++)
+            for (int i = 0; i < length; i++)
             {
-                Point p = new Point(x, y, sym);
+                Point p = new Point(tail);
+                p.Move(i, direction);
                 pList.Add(p);
             }
         }
